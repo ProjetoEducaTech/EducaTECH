8164ec5038af0423a2f696efe3b259c3d59975bc
VictorGabrielZampieri <victor.zampieri@alunos.sc.senac.br>
2022-01-26 10:51:00 -0300

    Foi adicionadas as anotações do hibernate no usuario

--- src/main/java/sistema/modelo/entidade/Usuario.java
+++ src/main/java/sistema/modelo/entidade/Usuario.java
@@ -1,21 +1,53 @@
 package sistema.modelo.entidade;
 import java.util.ArrayList;
-import java.util.List;
 
-public class Usuario {
+import java.util.List;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable{
 	
-	private String nome;
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "cpf_usuario" , length = 14, nullable = false, unique = true)
 	private String cpf;
+	
+	@Column(name = "nome_usuario", length = 25, nullable = false, unique = false)
+	private String nome;
+	
+	@Column(name = "senha_usuario", length = 20, nullable = false, unique = false)
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name = "crusos_favoritos", joinColumns = @JoinColumn(name = "cpf_usuario"), inverseJoinColumns = @JoinColumn(name = "id_curso"))
 	private List<Curso> cursosFavoritos;
 	
-	public Usuario(String nome, String cpf,String senha ) {
+	public Usuario() {}
+	
+	public Usuario(String cpf, String nome,String senha ) {
 		
-		setNome(nome);
 		setCpf(cpf);
+		setNome(nome);
 		setSenha(senha);
 		
 		cursosFavoritos = new ArrayList<Curso>();
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
 	}
 
 	public String getNome() {
@@ -26,14 +58,6 @@ public class Usuario {
 		this.nome = nome;
 	}
 
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
 	public String getSenha() {
 		return senha;
 	}
@@ -42,12 +66,14 @@ public class Usuario {
 		this.senha = senha;
 	}
 	
-	/*public boolean inserirCursoFavorito(Curso cursosFavoritos) {
-		return cursosFavoritos.add;
-	}*/
-
 	public List<Curso> recuperarCursosFavoritos() {
 		return cursosFavoritos;
 	}
+	
+	public void inserirCursoFavorito(Curso cursosFavoritos) {
+		 this.cursosFavoritos.add(cursosFavoritos);
+	}
+
+	
 
 }
